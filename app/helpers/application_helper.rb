686221996dad860cf3046283a19f84a1016e7241
marcdysart <dysart.marc@gmail.com>
2014-11-25 12:56:09 -0700

    Allow public to view non-private wikis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     (redcarpet.render text).html_safe
   end
 
+      def my_name
+      :'Marc Dysart'
+    end
+
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,4 +2,9 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
 
   scope :visible_to, -> (user) { user ? all : where(private: false) }
+
+  after_initialize :wiki_init
+  def wiki_init
+    self.private ||= 'false'
+  end
 end
